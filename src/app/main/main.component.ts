ec53b0c8f8167639c70809a9585897ebf671f369
Mitchell Hayes <MitchSlevin@gmail.com>
2023-02-02 10:39:42 -0500

    Update main.component.ts
    Attempting to fix build errors by feeding the output to chat gpt.

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 
+interface FormData {
+  brewStyle: string;
+  date: string;
+}
+
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+  styleUrls: ['./main.component.scss']
 })
 export class MainComponent {
-  brewStyle: string;
-  date: string;
+  brewStyle: string = '';
+  date: string = '';
 
   onSubmit(formData) {
     console.log(formData);
